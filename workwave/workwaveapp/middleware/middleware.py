eca7e9527d1315a0965d6583c65fd273b2937306
kayson2021 <hiruyoki@gmail.com>
2023-09-13 10:18:17 +0800

    13092023 v1.1

--- workwave/workwaveapp/middleware/middleware.py
+++ workwave/workwaveapp/middleware/middleware.py
@@ -1,24 +1,33 @@
 from django.shortcuts import redirect, HttpResponse
+from workwaveapp import models
+
+
+class AppUser(object):
+
+    def __init__(self):
+        self.user = None
+
 
 class LoginMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
     
     def __call__(self, request):
-        if request.path_info == '/login/' or 'submit_login/':
-            return self.get_response(request)
 
+        request.appUser = AppUser()
+
+        if request.path_info in ('/login/', '/submit_login/'):
+            return self.get_response(request)
+        
         info_dict = request.session.get("info")
-        print(info_dict)
-        if info_dict:
+        print({"info_dict":info_dict})
+        if info_dict is not None:
+            user_id = info_dict['id']
+            user_object = models.UserRegistration.objects.filter(id=user_id).first()
+            print({"user_object":user_object})
+            request.appUser.user = user_object.email
+            print({"request.appUser.user":request.appUser.user})
             return self.get_response(request) 
+        
         return redirect('/login/')
 
-
-        
-"""
-    def process_response(self, request, response):
-        pass
-    
-        #return redirect('CL_application/')
-"""
